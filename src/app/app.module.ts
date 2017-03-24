efa13a27c6d0bba797af9dfaa1954945edf15cf5
marcoguilherme <mgbm13@gmail.com>
2017-03-24 18:16:14 -0300

    Adicionado InAppBrowser

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { InAppBrowser } from '@ionic-native/in-app-browser'
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -22,6 +23,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   providers: [
     StatusBar,
     SplashScreen,
+    InAppBrowser,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 import { NavController, LoadingController } from 'ionic-angular';
-import { InAppBrowser } from 'ionic-native';
+import { InAppBrowser } from '@ionic-native/in-app-browser';
 
 import { Http } from '@angular/http';
 
@@ -13,14 +13,18 @@ import 'rxjs/add/operator/map';
 })
 export class HomePage {
 
-  public feeds: Object;
+  public feeds: any;
   private url: string = "https://www.reddit.com/new.json";
+  private olderPosts: string = "https://www.reddit.com/new.json?after=";
+  private newerPosts: string = "https://www.reddit.com/new.json?before=";
 
-  constructor(public navCtrl: NavController, public http: Http, public loadingCtrl: LoadingController) {
+  constructor(public navCtrl: NavController, public http: Http, public loadingCtrl: LoadingController, public iab: InAppBrowser) {
 
     this.fetchContent();
+
   }
 
+  //Lista o conteudo utilizando o loader
   fetchContent():void {
     let loading = this.loadingCtrl.create({
       content: 'Loading...'
@@ -33,12 +37,54 @@ export class HomePage {
       .map(res => res.json())
       .subscribe( data => {
         this.feeds = data.data.children;
+        this.changeThumbnail(this.feeds);
         loading.dismiss();
       });
+      
   }
 
-  itemSelected (url: string):void {
-    let browser = new InAppBrowser(url, '_system');
+  //Abre conteudo da url ema nova guia do browser
+  itemSelected (url: string) {
+
+    this.iab.create(url, '_system');
+
   }
+
+  changeThumbnail(feeds){
+    feeds.forEach((e, i, a) => {
+      if (!e.data.thumbnail || e.data.thumbnail.indexOf('b.thumbs.redditmedia.com') === -1 ) { 
+        e.data.thumbnail = 'http://www.redditstatic.com/icon.png';
+      }
+    })
+  }
+  
+  //Comando para recarregar novo conteudo
+    doRefresh(refresher) {
+ 
+    let paramsUrl = this.feeds[0].data.name;
+ 
+    this.http.get(this.newerPosts + paramsUrl).map(res => res.json())
+      .subscribe(data => {
+      
+        this.feeds = data.data.children.concat(this.feeds);
+
+        this.changeThumbnail(this.feeds);
+        
+        refresher.complete();
+      });
+  } 
+  
+  //Comando para fazer scroll infinito
+  doInfinite(infiniteScroll) {
+  
+    let paramsUrl = (this.feeds.length > 0) ? this.feeds[this.feeds.length - 1].data.name : "";
+     
+    this.http.get(this.olderPosts + paramsUrl).map(res => res.json())
+      .subscribe(data => {      
+        this.feeds = this.feeds.concat(data.data.children);
+        this.changeThumbnail(this.feeds);
+        infiniteScroll.complete();  
+      }, errors => console.log("")); 
+  }  
 
 }
